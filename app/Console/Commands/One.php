07bfcc1fc06fd42d5ca537cf1a8f6caddf388c33
qdskx <qdskx@outlook.com>
2020-05-13 15:30:18 +0800

    '新分支未提交,先合入master,在master提交'

--- app/Console/Commands/One.php
+++ app/Console/Commands/One.php
@@ -1,15 +1,15 @@
-<?php 
+<?php
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Inspiring;
 use Log;
 
-class Test extends Command {
+class One extends Command {
 
-    protected $name = 'test';//命令名称rrrrrr
+    protected $name = 'second';//命令名称rrrrrr
 
-    protected $description = '测试'; // 命令描述，没什么用
+    protected $description = 'second_one_command'; // 命令描述，没什么用
 
     /**
      * Execute the console command.
@@ -19,8 +19,8 @@ class Test extends Command {
     public function handle()
     {
         log::info('test');
-        echo 'rtttt';
-            // 功能代码写到这里
+        echo 'second_one_command';
+        // 功能代码写到这里
     }
 
 }
